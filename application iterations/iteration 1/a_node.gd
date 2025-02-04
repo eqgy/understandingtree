070f0e71cacfba1a20343ccbc4befa6a35732e20
sydney8howell <sthowell8@icloud.com>
2025-02-04 16:57:32 -0500

    Fixed positioning for creating new branches. Also updated line positioning, although there are still errors

--- application iterations/iteration 1/a_node.gd
+++ application iterations/iteration 1/a_node.gd
@@ -17,14 +17,11 @@ func add_line():
 	if parent != null:
 		if has_line == false:
 			if parent is Node2D && "too" in parent:
-				var d = (parent.global_position - global_position) /10
 				var line = Line2D.new()
-				var p = parent.global_position
-				line.add_point(p)
-				for i in range(0,10):
-					p += d
-					line.add_point(p)
+				line.add_point(global_position)
+				line.add_point(parent.global_position)
 				add_child(line)
+				
 			has_line = true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -34,9 +31,10 @@ func _process(delta):
 
 func _on_addbranch_pressed():
 	var child = load("res://a node.tscn").instantiate()
-	child.global_position.y += 100*children
-	child.global_position.x = global_position.x + 400
+	child.position.y += 100*children
+	child.position.x  += 400
 	child.add_line()
 	children += 1
 	add_child(child)
+	print(children)
 	pass # Replace with function body.
