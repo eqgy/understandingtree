64768ab148c8e764184c7d54cabb9b3c175d7a55
eqgy <aliakeplinger@icloud.com>
2025-02-18 14:05:34 -0500

    button deselects upon extraneous click

--- application iterations/iteration 1/leaf.gd
+++ application iterations/iteration 1/leaf.gd
@@ -1,22 +1,21 @@
 extends Button
-var selected = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
+func _input(event):
+	if button_pressed == true:
+		print("selected")
+		if event is InputEventMouseButton and event.pressed:
+			await get_tree().create_timer(.05).timeout
+			emit_signal("toggled", button_pressed)
+			button_pressed = false
+			print("tried to toggle ")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if selected == true:
-		if Input.is_action_just_pressed("mouse_click"):
-			await get_tree().create_timer(.05).timeout
-			emit_signal("toggled")
-			print("tried to toggle ")
-		
 	pass
 
-
-func _on_toggled(toggled_on):
-	selected = not selected
-	print(selected)
+	
+	
